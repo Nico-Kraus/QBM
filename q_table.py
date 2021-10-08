17df924b8522f9882755a74e627f11e91e88e0e2
ri27jep <n.kraus@campus.lmu.de>
2021-10-08 22:57:51 +0200

    qtable with positive init

--- q_table.py
+++ q_table.py
@@ -5,13 +5,13 @@ class Q_table():
         self.n_states = n_states
         self.lr = params["lr"]
         self.q_table = []
-        self.init_q_table()
+        self.init_q_table(float(params["init"]))
 
-    def init_q_table(self):
+    def init_q_table(self, init):
         for _ in range(self.n_states):
             actions = []
             for _ in range(self.n_actions):
-                actions.append(0.0)
+                actions.append(init)
             self.q_table.append(actions)
     
     def Q(self, state, action):
