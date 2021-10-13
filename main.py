aa76e073193628368aaa4a37e664b9e8bf23d7d7
Nico Kraus <n.kraus@campus.lmu.de>
2021-10-13 14:29:51 +0200

    new test

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ if __name__ == '__main__':
     for agent_params in params["agents"]:
         agent = Agent(env, agent_params)
         # scores, eps_history = play_eps_greedy(env, agent, params["play"]["num_samples"], params["play"]["max_steps"], env.start)
-        scores, eps_history = play_eps_greedy_rounds(env, agent, params["play"]["num_samples"], params["play"]["max_steps"], [[2,0],[8,0],[6,8],[0,10],[0,0]])
-        # scores = play_rdn_sample(env, agent, params["play"]["num_samples"], params["play"]["max_steps"], env.start)
+        # scores, eps_history = play_eps_greedy_rounds(env, agent, params["play"]["num_samples"], params["play"]["max_steps"], [[2,0],[8,0],[6,8],[0,10],[0,0]])
+        scores = play_rdn_sample(env, agent, params["play"]["num_samples"], params["play"]["max_steps"], env.start)
         compare_scores.append(scores)
 
     filename = create_filename_compare(env, params["agents"])
-    compare_learning_curves(compare_scores, filename, params["agents"], avg = 100)
-    #compare_learning_curves_rdn(compare_scores, filename, params["agents"], avg = 1)
+    # compare_learning_curves(compare_scores, filename, params["agents"], avg = 100)
+    compare_learning_curves_rdn(compare_scores, filename, params["agents"], avg = 1)
